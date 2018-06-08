1147fd7899f2d8de94518c5d1b6c73706f98c0db
Vinay M <rmdort@gmail.com>
2018-06-08 16:42:10 +0800

    Support generation of init file

---
+++ webpack.config.initfile.js
@@ -0,0 +1,80 @@
+var path = require('path');
+var webpack = require('webpack');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'olasearch.init.min.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'olasearch.core.min.css',
+      disable: false,
+      allChunks: true
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'OLA_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new webpack.ProvidePlugin({
+      'React': 'react'
+    }),
+    new webpack.BannerPlugin({ banner: "Copyright Ola Search Pte Ltd.", raw: false, entryOnly: true }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+  module: {
+    rules: [{
+      test: /\.js?/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+      include: [
+        path.join(__dirname, 'src')
+      ],
+    },
+    {
+      test: /(\.scss|\.css)$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [
+              require('autoprefixer')({browsers: ['last 3 versions', 'iOS 9']}),
+            ]
+          }
+        }, 'sass-loader']
+      
+    }
+    ]
+  },
+  externals: {
+    moment: 'moment',
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "@olasearch/core": "OlaSearch",
+    'olasearchconfig': 'OlaSearchConfig',
+    "olasearch": "OlaSearch",
+    "ramda": "R",
+    "redux": "Redux",
+    "react-redux": "ReactRedux"
+  },
+  resolve: {
+    alias: {
+      'olasearchconfig': path.join(__dirname, './src/olasearch.config')
+    },
+    modules: [
+      'node_modules', path.resolve(__dirname, './node_modules')
+    ]
+  }
+}
